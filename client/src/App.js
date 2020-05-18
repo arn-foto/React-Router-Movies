6a39cfdb38060b0a76fa6ac1dcfe28d3a5ee17c9
arn-foto <38259824+arn-foto@users.noreply.github.com>
2020-05-18 12:59:25 -0700

    Step 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
 
 const App = () => {
-  const [savedList, setSavedList] = useState([]);
-  const [movieList, setMovieList] = useState([]);
+	const [ savedList, setSavedList ] = useState([]);
+	const [ movieList, setMovieList ] = useState([]);
 
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
-          setMovieList(response.data);
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    getMovies();
-  }, []);
+	useEffect(() => {
+		const getMovies = () => {
+			axios
+				.get('http://localhost:5000/api/movies')
+				.then((response) => {
+					setMovieList(response.data);
+				})
+				.catch((error) => {
+					console.error('Server Error', error);
+				});
+		};
+		getMovies();
+	}, []);
 
-  const addToSavedList = movie => {
-    setSavedList([...savedList, movie]);
-  };
+	const addToSavedList = (movie) => {
+		setSavedList([ ...savedList, movie ]);
+	};
 
-  return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+	return (
+		<Router>
+			{' '}
+			<div>
+				<SavedList list={savedList} />
+				<div>Replace this Div with your Routes</div>
+			</div>
+		</Router>
+	);
 };
 
 export default App;
